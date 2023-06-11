6109849ef7370f85b078ba0ca7d0287eb9f03487
vadim <89281779909@mail.ru>
2023-06-11 20:21:42 +0300

    hw2.2

--- hw2/task2.java
+++ hw2/task2.java
@@ -18,7 +18,8 @@ public class task2 {
         int[] intArray = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 
         try {
-            int d = 0;
+            // int d = 0;
+            int d = 3;
             double catchedRes1 = intArray[8] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException e) {
